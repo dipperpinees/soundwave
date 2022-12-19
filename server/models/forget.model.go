01b6f1aa1222d1e8268024b8905fab93881d2de6
hiepnguyen223 <hiepnguyenn223@gmail.com>
2022-12-19 21:07:45 +0700

    add foreign key forget table

--- server/models/forget.model.go
+++ server/models/forget.model.go
@@ -5,6 +5,7 @@ import "time"
 type Forget struct {
 	ID        uint `gorm:"primaryKey;autoIncrement" json:"id"`
 	UserID    uint
+	User      User
 	Code      string
 	CreatedAt time.Time `gorm:"autoCreateTime;column:created_at" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime;column:updated_at" json:"updated_at"`
